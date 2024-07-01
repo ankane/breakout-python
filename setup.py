4c6d20ee1e86888494e939dfe7876924643f5f11
Andrew Kane <andrew@ankane.org>
2024-06-30 17:32:26 -0700

    Version bump to 0.2.0 [skip ci]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ ext_modules = [
 
 setup(
     name='breakout-detection',
-    version='0.1.0',
+    version='0.2.0',
     description='Breakout detection for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
